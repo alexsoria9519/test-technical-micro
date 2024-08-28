ab498acd731141838f39aebe0ed5460f6c3da26b
Alex Soria <alex.soria@corebitsdev.com>
2024-08-28 14:05:31 -0500

    feat: initial commit person-client microservices

---
+++ person-client-microservices/src/main/java/com/example/personclientmicroservices/PersonClientMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.personclientmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonClientMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonClientMicroservicesApplication.class, args);
+    }
+
+}
---
+++ person-client-microservices/src/test/java/com/example/personclientmicroservices/PersonClientMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.personclientmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonClientMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
